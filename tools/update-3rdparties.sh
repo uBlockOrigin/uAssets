163eaa5aad6f771892542be77f258aa2ef141f7c
Raymond Hill <rhill@raymondhill.net>
2018-01-21 09:16:30 -0500

    add 3rd-party assets for uMatrix and update all 3rd-party assets

--- tools/update-3rdparties.sh
+++ tools/update-3rdparties.sh
@@ -10,9 +10,12 @@ declare -A assets
 assets=(
     ['thirdparties/easylist-downloads.adblockplus.org/easylist.txt']='https://easylist.to/easylist/easylist.txt'
     ['thirdparties/easylist-downloads.adblockplus.org/easyprivacy.txt']='https://easylist.to/easylist/easyprivacy.txt'
+    ['thirdparties/hosts-file.net/ad_servers.txt']='http://hosts-file.net/.%5Cad_servers.txt'
     ['thirdparties/mirror1.malwaredomains.com/files/justdomains']='https://mirror1.malwaredomains.com/files/justdomains'
     ['thirdparties/pgl.yoyo.org/as/serverlist']='https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=1&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=&mimetype=plaintext'
     ['thirdparties/publicsuffix.org/list/effective_tld_names.dat']='https://publicsuffix.org/list/public_suffix_list.dat'
+    ['thirdparties/someonewhocares.org/hosts/hosts.txt']='http://someonewhocares.org/hosts/hosts'
+    ['thirdparties/winhelp2002.mvps.org/hosts.txt']='http://winhelp2002.mvps.org/hosts.txt'
     ['thirdparties/www.malwaredomainlist.com/hostslist/hosts.txt']='https://www.malwaredomainlist.com/hostslist/hosts.txt'
 )
 
