2f6d932fa7f7bd05f4a9f4075fffe421b07aaeac
Raymond Hill <rhill@raymondhill.net>
2022-03-17 13:09:42 -0400

    Maybe fix new gh-pages CI

--- tools/make-gh-pages.sh
+++ tools/make-gh-pages.sh
@@ -10,12 +10,14 @@ echo "*** Created temporary directory: $DES"
 echo "*** Copying files to $DES"
 cp ./filters/*.txt $DES/
 echo "*** Checking out gh-pages"
-git fetch origin gh-pages --depth=1
-git checkout origin/gh-pages
+git fetch origin
+git checkout gh-pages
 echo "*** Copying files to gh-pages"
 cp $DES/*.txt ./
 if [[ -n $(git diff) ]]; then
   echo "*** Committing changes to gh-pages"
+  git config --global user.email "uassets-ci@raymondhill.net"
+  git config --global user.name "uAssets CI"
   git add -u
   git commit -m 'Update all lists'
   git push origin gh-pages
