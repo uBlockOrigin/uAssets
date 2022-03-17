ebdea125aebf2e5484231cd9e2ae64f2730e89fc
Raymond Hill <rhill@raymondhill.net>
2022-03-17 13:10:57 -0400

    Maybe fix new gh-pages CI

--- tools/make-gh-pages.sh
+++ tools/make-gh-pages.sh
@@ -16,8 +16,8 @@ echo "*** Copying files to gh-pages"
 cp $DES/*.txt ./
 if [[ -n $(git diff) ]]; then
   echo "*** Committing changes to gh-pages"
-  git config --global user.email "uassets-ci@raymondhill.net"
-  git config --global user.name "uAssets CI"
+  git config user.email "uassets-ci@raymondhill.net"
+  git config user.name "uAssets CI"
   git add -u
   git commit -m 'Update all lists'
   git push origin gh-pages
