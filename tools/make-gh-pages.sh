39906f1d004a06dc047cef519af6da740f7ace0d
Raymond Hill <rhill@raymondhill.net>
2022-03-17 10:32:24 -0400

    Set up a gh-pages dedicated to host uBO's own filter lists

---
+++ tools/make-gh-pages.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# This script assumes a linux environment
+
+set -e
+
+DES=$(mktemp -d)
+echo "*** Created temporary directory: $DES"
+
+echo "*** Copying files to $DES"
+cp ./filters/*.txt $DES/
+echo "*** Checking out gh-pages"
+git checkout gh-pages
+echo "*** Copying files to gh-pages"
+cp $DES/*.txt ./
+if [[ -n $(git diff) ]]; then
+  echo "*** Committing changes to gh-pages"
+  git add -u
+  git commit -m 'Update all lists'
+  git push origin gh-pages
+else
+  echo "*** No changes to commit"
+fi
+
+echo "*** Removing temporary directory"
+rm -f $DES/*.txt
+rmdir $DES
+
+echo "*** Checking out master"
+git checkout master
