67856deeb5152bea00132c102cfeadc7e39738f6
Raymond Hill <rhill@raymondhill.net>
2023-10-24 19:41:07 -0400

    Add fields to diff-patachable lists

--- tools/make-ublock.sh
+++ tools/make-ublock.sh
@@ -5,6 +5,9 @@
 echo "*** uAssets: Assembling filters/filters.txt"
 node ./tools/easylist/make-easylist.js in=templates/ublock-filters.template out=filters/filters.min.txt minify=1
 
+echo "*** uAssets: Assembling filters/quick-fixes.txt"
+node ./tools/easylist/make-easylist.js in=templates/ublock-quick-fixes.template out=filters/quick-fixes.min.txt minify=1
+
 echo "*** uAssets: Assembling filters/privacy.txt"
 node ./tools/easylist/make-easylist.js in=templates/ublock-privacy.template out=filters/privacy.min.txt minify=1
 
