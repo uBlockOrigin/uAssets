96fa8484438e3350d5e71640242503f2b8dae2bc
Raymond Hill <rhill@raymondhill.net>
2023-11-08 18:27:14 -0500

    Use `mjs` instead of `js` to signal js module
    This way no need for package.json.

--- tools/make-easylist.sh
+++ tools/make-easylist.sh
@@ -9,27 +9,27 @@ git clone --depth 1 https://github.com/easylist/easylist.git $TMPDIR/easylist
 cp -R templates/easy*.template $TMPDIR/easylist/
 
 echo "*** uAssets: Assembling easylist.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist.template out=thirdparties/easylist/easylist.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist.template out=thirdparties/easylist/easylist.txt
 
 echo "*** uAssets: Assembling easyprivacy.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easyprivacy.template out=thirdparties/easylist/easyprivacy.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easyprivacy.template out=thirdparties/easylist/easyprivacy.txt
 
 echo "*** uAssets: Assembling easylist-annoyances.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist-annoyances.template out=thirdparties/easylist/easylist-annoyances.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist-annoyances.template out=thirdparties/easylist/easylist-annoyances.txt
 
 echo "*** uAssets: Assembling easylist-cookies.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist-cookies.template out=thirdparties/easylist/easylist-cookies.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist-cookies.template out=thirdparties/easylist/easylist-cookies.txt
 
 echo "*** uAssets: Assembling easylist-social.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist-social.template out=thirdparties/easylist/easylist-social.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist-social.template out=thirdparties/easylist/easylist-social.txt
 
 echo "*** uAssets: Assembling easylist-newsletters.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist-newsletters.template out=thirdparties/easylist/easylist-newsletters.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist-newsletters.template out=thirdparties/easylist/easylist-newsletters.txt
 
 echo "*** uAssets: Assembling easylist-notifications.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist-notifications.template out=thirdparties/easylist/easylist-notifications.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist-notifications.template out=thirdparties/easylist/easylist-notifications.txt
 
 echo "*** uAssets: Assembling easylist-chat.txt"
-node ./tools/easylist/make-easylist.js dir=$TMPDIR/easylist in=easylist-chat.template out=thirdparties/easylist/easylist-chat.txt
+node ./tools/make-easylist.mjs dir=$TMPDIR/easylist in=easylist-chat.template out=thirdparties/easylist/easylist-chat.txt
 
 rm -rf $TMPDIR
--- tools/make-ublock.sh
+++ tools/make-ublock.sh
@@ -3,19 +3,19 @@
 # This script assumes a linux environment
 
 echo "*** uAssets: Assembling filters/filters.txt"
-node ./tools/easylist/make-easylist.js in=templates/ublock-filters.template out=filters/filters.min.txt minify=1
+node ./tools/make-easylist.mjs in=templates/ublock-filters.template out=filters/filters.min.txt minify=1
 
 echo "*** uAssets: Assembling filters/quick-fixes.txt"
-node ./tools/easylist/make-easylist.js in=templates/ublock-quick-fixes.template out=filters/quick-fixes.min.txt minify=1
+node ./tools/make-easylist.mjs in=templates/ublock-quick-fixes.template out=filters/quick-fixes.min.txt minify=1
 
 echo "*** uAssets: Assembling filters/privacy.txt"
-node ./tools/easylist/make-easylist.js in=templates/ublock-privacy.template out=filters/privacy.min.txt minify=1
+node ./tools/make-easylist.mjs in=templates/ublock-privacy.template out=filters/privacy.min.txt minify=1
 
 echo "*** uAssets: Assembling filters/unbreak.txt"
-node ./tools/easylist/make-easylist.js in=templates/ublock-unbreak.template out=filters/unbreak.min.txt minify=1
+node ./tools/make-easylist.mjs in=templates/ublock-unbreak.template out=filters/unbreak.min.txt minify=1
 
 echo "*** uAssets: Assembling filters/badware.txt"
-node ./tools/easylist/make-easylist.js in=templates/ublock-badware.template out=filters/badware.min.txt minify=1
+node ./tools/make-easylist.mjs in=templates/ublock-badware.template out=filters/badware.min.txt minify=1
 
 echo "*** uAssets: Assembling filters/annoyances.txt"
-node ./tools/easylist/make-easylist.js in=templates/ublock-annoyances.template out=filters/annoyances.min.txt minify=1
+node ./tools/make-easylist.mjs in=templates/ublock-annoyances.template out=filters/annoyances.min.txt minify=1
