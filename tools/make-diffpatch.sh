a8403c95ed5107aa4b809de306299acf917abfe3
Raymond Hill <rhill@raymondhill.net>
2023-11-08 11:30:39 -0500

    Make the patch-generating scripts repo-agnostic
    Additionally, lint scripts with shellcheck tool:
    https://www.shellcheck.net/

--- tools/make-diffpatch.sh
+++ tools/make-diffpatch.sh
@@ -4,9 +4,9 @@
 
 set -e
 
-# To be executed at the root of uAssetsCDN repo
+# To be executed at the root of CDN repo
 #
-# It's not being hosted at uAssetsCDN because that
+# It's not being hosted at CDN because that
 # repo is also used as a website
 
 VERSION=$1
@@ -15,34 +15,40 @@ if [[ -z $VERSION ]]; then
     exit 1
 fi
 
-PREVIOUS_VERSION=$(<version)
-PREVIOUS_PATCH_FILE="patches/${PREVIOUS_VERSION}.patch"
-> "${PREVIOUS_PATCH_FILE}"
+PATCHES_DIR=$2
+if [[ -z $PATCHES_DIR ]]; then
+    echo "Error: patches directory is not provided, aborting"
+    exit 1
+fi
 
-NEXT_PATCH_FILE="patches/${VERSION}.patch"
+PREVIOUS_VERSION=$(<version)
+PREVIOUS_PATCH_FILE="PATCHES_DIR/${PREVIOUS_VERSION}.patch"
+: > "${PREVIOUS_PATCH_FILE}"
+
+NEXT_PATCH_FILE="PATCHES_DIR/${VERSION}.patch"
 
 # Temporary file to receive the RCS patch data
 DIFF=$(mktemp)
 
-FILES=( $(git diff --name-only) )
+read -ar FILES < <(git diff --name-only)
 for FILE in "${FILES[@]}"; do
 
     # Reference:
     # https://github.com/ameshkov/diffupdates
 
-    if [[ -n $(grep '^! Version: ' <(head $FILE)) ]]; then
-        sed -Ei "1,10s;^! Version: .+$;! Version: $VERSION;" $FILE
+    if (head "$FILE" | grep -q '^! Version: '); then
+        sed -Ei "1,10s;^! Version: .+$;! Version: $VERSION;" "$FILE"
     fi
 
     # Patches are for filter lists supporting differential updates
-    if [[ -n $(grep '^! Diff-Path: ' <(head $FILE)) ]]; then
+    if (head "$FILE" | grep -q '^! Diff-Path: '); then
 
         # Extract diff name from `! Diff-Path:` field
-        DIFF_NAME=$(grep -m 1 -oP '^! Diff-Path: [^#]+#?\K.*' $FILE)
+        DIFF_NAME=$(grep -m 1 -oP '^! Diff-Path: [^#]+#?\K.*' "$FILE")
         # Fall back to `! Diff-Name:` field if no name found
         # Remove once `! Diff-Name:` is no longer needed after transition
         if [[ -z $DIFF_NAME ]]; then
-            DIFF_NAME=$(grep -m 1 -oP '^! Diff-Name: \K.+' $FILE)
+            DIFF_NAME=$(grep -m 1 -oP '^! Diff-Name: \K.+' "$FILE")
         fi
         echo "Info: Diff name for ${FILE} is ${DIFF_NAME}"
 
@@ -50,24 +56,24 @@ for FILE in "${FILES[@]}"; do
         if [[ -n $DIFF_NAME ]]; then
 
             # Compute relative patch path
-            PATCH_PATH=$(realpath --relative-to=$(dirname $FILE) $NEXT_PATCH_FILE)
+            PATCH_PATH="$(realpath --relative-to="$(dirname "$FILE")" "$NEXT_PATCH_FILE")"
 
             # Fill in patch path to next version (do not clobber hash portion)
-            sed -Ei "1,10s;^! Diff-Path: [^#]+(#.+)?$;! Diff-Path: $PATCH_PATH\1;" $FILE
+            sed -Ei "1,10s;^! Diff-Path: [^#]+(#.+)?$;! Diff-Path: $PATCH_PATH\1;" "$FILE"
 
             # Compute the RCS diff between current version and new version
-            git show HEAD:$FILE | diff -n - $FILE > $DIFF || true
+            git show "HEAD:$FILE" | diff -n - "$FILE" > "$DIFF" || true
 
-            FILE_CHECKSUM=$(sha1sum $FILE)
+            FILE_CHECKSUM="$(sha1sum "$FILE")"
             FILE_CHECKSUM=${FILE_CHECKSUM:0:10}
 
-            DIFF_LINES=$(wc -l < $DIFF)
+            DIFF_LINES="$(wc -l < "$DIFF")"
             echo "Info: Computed patch for ${FILE} has ${DIFF_LINES} lines"
 
             # Populate output file with patch information
             echo "Info: Adding patch data of ${FILE} to ${PREVIOUS_PATCH_FILE}"
-            echo "diff name:$DIFF_NAME lines:$DIFF_LINES checksum:$FILE_CHECKSUM" >> $PREVIOUS_PATCH_FILE
-            cat $DIFF >> $PREVIOUS_PATCH_FILE
+            echo "diff name:$DIFF_NAME lines:$DIFF_LINES checksum:$FILE_CHECKSUM" >> "$PREVIOUS_PATCH_FILE"
+            cat "$DIFF" >> "$PREVIOUS_PATCH_FILE"
 
         else
 
@@ -78,14 +84,14 @@ for FILE in "${FILES[@]}"; do
 
     # Stage changed file
     echo "Info: Staging ${FILE}"
-    git add -u $FILE
+    git add -u "$FILE"
 
 done
 
 echo "Info: Staging ${PREVIOUS_PATCH_FILE}"
-git add $PREVIOUS_PATCH_FILE
+git add "$PREVIOUS_PATCH_FILE"
 
 echo -n "$VERSION" > version
 git add version
 
-rm -f $DIFF
+rm -f "$DIFF"
--- tools/update-diffpatches.sh
+++ tools/update-diffpatches.sh
@@ -4,22 +4,38 @@
 
 set -e
 
-# To be executed at the root of uAssetsCDN repo
+# To be executed at the root of CDN repo
 #
-# It's not being hosted at uAssetsCDN because that
+# It's not being hosted at CDN because that
 # repo is also used as a website
 
+REPO_DIR=$1
+if [[ -z $REPO_DIR ]]; then
+    echo "Error: repo directory is not provided, aborting"
+    exit 1
+fi
+
+PATCHES_DIR=$2
+if [[ -z $PATCHES_DIR ]]; then
+    echo "Error: patches directory is not provided, aborting"
+    exit 1
+fi
+
+FILTER_FILES=$3
+if [[ -z $FILTER_FILES ]]; then
+    echo "Error: filter lists are not provided, aborting"
+    exit 1
+fi
+
+read -ar PATCH_FILES < <(ls -1v "$PATCHES_DIR/*.patch" | head -n -1)
+export PATCH_FILES
 
 # Keep only the most recent (5-day x 4-per-day) patches
-OBSOLETE_PATCHES=( $(ls -1v patches/*.patch | head -n -20) )
+read -ar OBSOLETE_PATCHES < <(ls -1v "$PATCHES_DIR/*.patch" | head -n -20)
 for FILE in "${OBSOLETE_PATCHES[@]}"; do
     echo "Removing obsolete patch $FILE"
-    git rm $FILE
+    git rm "$FILE"
 done
-
-
-# Revisit all patch files except the most recent one
-PATCH_FILES=( $(ls -1v patches/*.patch | head -n -1) )
 
 NEW_PATCH_FILE=$(mktemp)
 DIFF_FILE=$(mktemp)
@@ -27,23 +43,22 @@ DIFF_FILE=$(mktemp)
 for PATCH_FILE in "${PATCH_FILES[@]}"; do
 
     # Extract tag from patch file name
-    [[ ${PATCH_FILE} =~ ^patches/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\.patch$ ]] && \
+    [[ ${PATCH_FILE} =~ ^$PATCHES_DIR/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\.patch$ ]] && \
         PREVIOUS_VERSION=${BASH_REMATCH[1]}
 
     # This will receive a clone of an old version of the current repo
     echo "Fetching repo at $PREVIOUS_VERSION version"
     OLD_REPO=$(mktemp -d)
-    git clone -q --single-branch --branch $PREVIOUS_VERSION --depth=1 https://github.com/uBlockOrigin/uAssetsCDN.git "$OLD_REPO" 2>/dev/null
+    git clone -q --single-branch --branch "$PREVIOUS_VERSION" --depth=1 "https://github.com/$REPO_DIR.git" "$OLD_REPO" 2>/dev/null
 
-    > "$NEW_PATCH_FILE"
+    : > "$NEW_PATCH_FILE"
 
-    FILTER_LISTS=( $(ls -1 filters/*.min.txt) $(ls -1 thirdparties/*) )
-    for FILTER_LIST in "${FILTER_LISTS[@]}"; do
+    for FILTER_LIST in "${FILTER_FILES[@]}"; do
 
-        if [ ! -f $OLD_REPO/$FILTER_LIST ]; then continue; fi
+        if [ ! -f "$OLD_REPO/$FILTER_LIST" ]; then continue; fi
 
         # Patches are for filter lists supporting differential updates
-        if [[ -z $(grep '^! Diff-Path: ' <(head $OLD_REPO/$FILTER_LIST)) ]]; then
+        if ! (head "$OLD_REPO/$FILTER_LIST" | grep -q '^! Diff-Path: '); then
             continue
         fi
 
@@ -51,11 +66,11 @@ for PATCH_FILE in "${PATCH_FILES[@]}"; do
         # https://github.com/ameshkov/diffupdates
 
         # Extract diff name from `! Diff-Path:` field
-        DIFF_NAME=$(grep -m 1 -oP '^! Diff-Path: [^#]+#?\K.*' $FILTER_LIST)
+        DIFF_NAME=$(grep -m 1 -oP '^! Diff-Path: [^#]+#?\K.*' "$FILTER_LIST")
         # Fall back to `! Diff-Name:` field if no name found
         # Remove once `! Diff-Name:` is no longer needed after transition
         if [[ -z $DIFF_NAME ]]; then
-            DIFF_NAME=$(grep -m 1 -oP '^! Diff-Name: \K.+' $FILTER_LIST)
+            DIFF_NAME=$(grep -m 1 -oP '^! Diff-Name: \K.+' "$FILTER_LIST")
         fi
 
         # We need a patch name to generate a valid patch
@@ -65,16 +80,16 @@ for PATCH_FILE in "${PATCH_FILES[@]}"; do
         fi
 
         # Compute the RCS diff between current version and new version
-        diff -n $OLD_REPO/$FILTER_LIST $FILTER_LIST > "$DIFF_FILE" || true
+        diff -n "$OLD_REPO/$FILTER_LIST $FILTER_LIST" > "$DIFF_FILE" || true
 
-        FILE_CHECKSUM=$(sha1sum $FILTER_LIST)
+        FILE_CHECKSUM=$(sha1sum "$FILTER_LIST")
         FILE_CHECKSUM=${FILE_CHECKSUM:0:10}
 
-        DIFF_LINE_COUNT=$(cat "$DIFF_FILE" | wc -l)
+        DIFF_LINE_COUNT=$(wc -l "$DIFF_FILE")
 
         # Patch header
         DIFF_HEAD="diff name:$DIFF_NAME lines:$DIFF_LINE_COUNT checksum:$FILE_CHECKSUM"
-        echo "\tAdding diff: $DIFF_HEAD"
+        printf "\tAdding diff: %s\n" "$DIFF_HEAD"
         echo "$DIFF_HEAD" >> "$NEW_PATCH_FILE"
         # Patch data
         cat "$DIFF_FILE" >> "$NEW_PATCH_FILE"
@@ -87,7 +102,7 @@ for PATCH_FILE in "${PATCH_FILES[@]}"; do
     mv -f "$NEW_PATCH_FILE" "$PATCH_FILE"
     ls -l "$PATCH_FILE"
     echo "Info: Staging ${PATCH_FILE}"
-    git add -u $PATCH_FILE
+    git add -u "$PATCH_FILE"
 
 done
 
