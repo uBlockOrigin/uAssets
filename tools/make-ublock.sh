9de3ab4616f37bb4cc39e088d0ec0dd9ba77fa24
daylight <starcrush@monemail.com>
2024-11-01 13:27:03 +0300

    Compress up uBlock script

--- tools/make-ublock.sh
+++ tools/make-ublock.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 
-echo "*** uAssets: Assembling filters/filters.txt"
-node ./tools/make-easylist.mjs in=templates/ublock-filters.template out=filters/filters.min.txt minify=1
+declare -A filters=(
+    ["filters.txt"]="ublock-filters.template"
+    ["quick-fixes.txt"]="ublock-quick-fixes.template"
+    ["privacy.txt"]="ublock-privacy.template"
+    ["unbreak.txt"]="ublock-unbreak.template"
+    ["badware.txt"]="ublock-badware.template"
+    ["annoyances.txt"]="ublock-annoyances.template"
+)
 
-echo "*** uAssets: Assembling filters/quick-fixes.txt"
-node ./tools/make-easylist.mjs in=templates/ublock-quick-fixes.template out=filters/quick-fixes.min.txt minify=1
-
-echo "*** uAssets: Assembling filters/privacy.txt"
-node ./tools/make-easylist.mjs in=templates/ublock-privacy.template out=filters/privacy.min.txt minify=1
-
-echo "*** uAssets: Assembling filters/unbreak.txt"
-node ./tools/make-easylist.mjs in=templates/ublock-unbreak.template out=filters/unbreak.min.txt minify=1
-
-echo "*** uAssets: Assembling filters/badware.txt"
-node ./tools/make-easylist.mjs in=templates/ublock-badware.template out=filters/badware.min.txt minify=1
-
-echo "*** uAssets: Assembling filters/annoyances.txt"
-node ./tools/make-easylist.mjs in=templates/ublock-annoyances.template out=filters/annoyances.min.txt minify=1
+for filter in "${!filters[@]}"; do
+    echo "*** uAssets: Assembling filters/$filter"
+    node ./tools/make-easylist.mjs in="templates/${filters[$filter]}" out="filters/$filter" minify=1
+done
